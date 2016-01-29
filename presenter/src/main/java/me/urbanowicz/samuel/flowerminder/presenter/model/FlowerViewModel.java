a9d67c0549d1fffca2422c2fb37d267b93347b4a
Samuel Urbanowicz <samiuelson@gmail.com>
2016-01-29 16:22:50 +0100

    Implement simple Flower view model

--- presenter/src/main/java/me/urbanowicz/samuel/flowerminder/presenter/model/FlowerViewModel.java
+++ presenter/src/main/java/me/urbanowicz/samuel/flowerminder/presenter/model/FlowerViewModel.java
@@ -1,4 +1,23 @@
 package me.urbanowicz.samuel.flowerminder.presenter.model;
 
+import me.urbanowicz.samuel.flowerminder.domain.entity.FlowerEntity;
+
 public class FlowerViewModel {
+    String name;
+
+    public FlowerViewModel(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static FlowerViewModel from(FlowerEntity flowerEntity) {
+        return new FlowerViewModel(flowerEntity.getName());
+    }
 }
