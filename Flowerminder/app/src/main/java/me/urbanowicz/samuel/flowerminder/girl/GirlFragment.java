95438d014f688ef308ce541c02d1c28315bcd0a5
samiuelson <samiuelson@gmail.com>
2016-06-15 17:24:00 +0200

    Add minimal layout for girl screen

--- Flowerminder/app/src/main/java/me/urbanowicz/samuel/flowerminder/girl/GirlFragment.java
+++ Flowerminder/app/src/main/java/me/urbanowicz/samuel/flowerminder/girl/GirlFragment.java
@@ -3,6 +3,11 @@ package me.urbanowicz.samuel.flowerminder.girl;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.urbanowicz.samuel.flowerminder.R;
 
 public class GirlFragment extends Fragment implements GirlContract.View {
 
@@ -16,6 +21,12 @@ public class GirlFragment extends Fragment implements GirlContract.View {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.girl_fragment, container, false);
     }
 
     @Override
